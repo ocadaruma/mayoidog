c44ebe99e4daa55950a92e678ef67082a71c2d89
kgmyshin <kgmyshin82@gmail.com>
2014-03-08 14:43:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mayoidog::Application.config.secret_key_base = 'ae80b746db8efb3e0340cfdd5163afd972738c444316b084772418e0b53d5001f91eb999053da9584567bc91f2dc627cf8d2ec19bc558e66bbaf27257a283df3'
